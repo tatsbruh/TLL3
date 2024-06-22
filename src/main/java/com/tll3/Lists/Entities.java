0cabea8a8ecadd1c66d7ffb5df05b1f641886d8b
tatsushiri <pierovelazco017@gmail.com>
2024-06-22 14:52:07 -0500

    me cago en la puta

--- src/main/java/com/tll3/Lists/Entities.java
+++ src/main/java/com/tll3/Lists/Entities.java
@@ -40,10 +40,8 @@ import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.FireworkMeta;
 import org.bukkit.inventory.meta.PotionMeta;
-import org.bukkit.potion.PotionData;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
-import org.bukkit.potion.PotionType;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import java.lang.reflect.Field;
